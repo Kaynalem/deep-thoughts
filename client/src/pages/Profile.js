80125eab4c44d6fb2d23b12c1517db8833f5910f
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-08-08 07:40:38 -0500

    add user mutations

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_USER } from '../utils/queries';
-
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
-
+  const user = data?.me || data?.user || {};
+  // redirect to personal profile page if username is the logged-in user's
+  //code should be if (Auth.loggedIn() && Auth.getProfile().data.username.toLowerCase() === userParam.toLowerCase()) {
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
   if (loading) {
     return <div>Loading...</div>;
   }
